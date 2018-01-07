4e17034cdce8513fd4cd93ccf51adfcbf413d315
Parinya Punpongsanon <parinya@mit.edu>
2018-01-07 16:08:44 -0500

    change the button position.

--- js/lightbox.js
+++ js/lightbox.js
@@ -5,7 +5,8 @@ function openModal()
 }
 
 // Close the Modal
-function closeModal() {
+function closeModal() 
+{
   document.getElementById('lightbox-modal').style.display = "none";
 }
 
@@ -13,16 +14,19 @@ var slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
-function plusSlides(n) {
+function plusSlides(n) 
+{
   showSlides(slideIndex += n);
 }
 
 // Thumbnail image controls
-function currentSlide(n) {
+function currentSlide(n) 
+{
   showSlides(slideIndex = n);
 }
 
-function showSlides(n) {
+function showSlides(n) 
+{
   var i;
   var slides = document.getElementsByClassName("lb-slides");
   var dots = document.getElementsByClassName("demo");
